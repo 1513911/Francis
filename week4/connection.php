2841d366f1133c698679b2196a852879fa89186d
a <a>
2016-03-02 17:23:24 +0000

    k

--- week4/connection.php
+++ week4/connection.php
@@ -5,7 +5,7 @@
  * Date: 02/03/2016
  * Time: 15:51
  */
-<?php
+
 define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
 define('DB_USERNAME', 'bf10131484f6ac');
 define('DB_PASSWORD', 'b7510474');
