4031992294a92be688fc5517571b8e695c3ec79a
a <a>
2016-03-02 17:20:05 +0000

    k

--- week4/connection.php
+++ week4/connection.php
@@ -6,9 +6,9 @@
  * Time: 15:51
  */
 <?php
-define('DB_SERVER', 'weblab');
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
 define('DB_USERNAME', 'bf10131484f6ac');
 define('DB_PASSWORD', 'b7510474');
-define('DB_DATABASE', 'Database=weblab;Data Source=ap-cdbr-azure-east-c.cloudapp.net;User Id=bf10131484f6ac;Password=b7510474');
+define('DB_DATABASE', 'weblab');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
