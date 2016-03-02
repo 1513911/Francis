da53e9f009d61e6150813e42e3abc8e73fc9a24f
a <a>
2016-03-02 17:01:26 +0000

    k

---
+++ week4/Home.php
@@ -0,0 +1,18 @@
+
+/**
+ * Created by PhpStorm.
+ * User: 1513911
+ * Date: 02/03/2016
+ * Time: 17:00
+ */
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+<h1>Hello</h1>
+</body>
+</html>
