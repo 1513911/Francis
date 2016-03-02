e69918e44051eb00975338c197c177949233eeca
a <a>
2016-03-02 17:21:07 +0000

    k

--- week4/login.php
+++ week4/login.php
@@ -8,11 +8,11 @@
 
 
 include("connection.php"); //Establishing connection with our database
-?>
+
 
 if(empty($_POST["username"]) || empty($_POST["password"]))
 {
-$echo = "Both fields are required.";
+echo "Both fields are required.";
 }else
 {
 $username=$_POST['username'];
@@ -24,7 +24,7 @@ if(mysqli_num_rows($result) == 1)
 header("location: home.php"); // Redirecting To another Page
 }else
 {
-$echo "Incorrect username or password.";
+echo "Incorrect username or password.";
 }
 }
 
