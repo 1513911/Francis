e6f22f2827feef2bff32b2d026fcb12efcb4dcb3
1513911 <1513911@rgu.ac.uk>
2016-03-23 15:48:52 +0000

    Update Home.php

--- week4/Home.php
+++ week4/Home.php
@@ -13,27 +13,8 @@ include("check.php");
 <body>
 <h1 class="hello">Hello, <em><?php echo $login_user;?>!</em></h1>
 <br><br><br>
-<a href="logout.php" style="font-size:18px">Logout</a>
+
 <br>
-<div id="content"
-    
-     //Establish connection with our database
-     include("connection.php");
-     //select everything from our bugs table where the ID is right
-     $sql="SELECT*FROM bugs WHERE bugs.ID = "$_GET["id"];
-     //fetch our result from the database
-     $result=mysqli_query($db,$sql);
-     //we scan through each row in the response
-     $row = mysqli_fetch_assoc($result);
-     //get the title and id from the bug
-     $bugTitle = $row['title'];
-     $bugID = $row['ID'];
-     $bugDesc = $row['desc'];
-
-     echo "<h2>".$bugTitle."</h2>
-     echo "<p>".$bugDesc."</p>";
-
-
-    </div>
+<a href="logout.php" style="font-size:18px">Logout</a>
 </body>
 </html>
