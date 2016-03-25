c1233cc7acb32334342f98d24ad26a7b99ebbc34
1513911 <1513911@rgu.ac.uk>
2016-03-25 19:55:01 +0000

    Update buildbug.php

--- week4/buildbug.php
+++ week4/buildbug.php
@@ -15,5 +15,5 @@ $fixedDate = $_POST["datefixed"];
 $sql = "INSERT INTO bugs(title, desc, postDate, fixedDate) VALUES('$title','$description', '$postDate', '$fixedDate')";
 mysqli_query($db,$sql);
  
-echo "I am here";
+echo "File has been posted!";
 
