da883137b1eaf141bc82f5822d3dc83186362499
1513911 <1513911@rgu.ac.uk>
2016-03-25 12:08:35 +0000

    Update register.php

--- week4/register.php
+++ week4/register.php
@@ -49,6 +49,16 @@
 					</td>
 				</tr>
 				<tr>
+					<td height="23" style="font-weight: bold">
+							<div align="right">
+								<label for="phone">Phone Extension</label>
+							</div>
+					</td>
+					<td>
+							<input name="phone" type="text" class="input" size="25" required />
+					</td>
+				</tr>
+				<tr>
 					<td height="23"></td>
 					<td>
 						<div align="right">
