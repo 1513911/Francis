76f480d39c2c8491e7acd0a1f8b6b96e3b2eb532
1513911 <1513911@rgu.ac.uk>
2016-03-25 13:24:01 +0000

    Update buildbug.php

--- week4/buildbug.php
+++ week4/buildbug.php
@@ -1,11 +1,21 @@
 <?php 
-include("connection.php")
+include("connection.php");
+
 $title = $_POST["title"];
 $description = $_POST["description"];
-$postDate = $_POST["postdate"];
-$fixedDate = $_POST["fixeddat"];
+$postDate = $_POST["dateposted"];
+$fixedDate = $_POST["datefixed"];
 
 $sql = INSERT INTO bugs(title, desc, postDate, fixedDate) VALUES('$title','$description', '$postDate', '$fixedDate');
 mysqli_query($db,$sql);
 
 ?>
+<!DOCTYPE html>
+<html lang = "en">
+  <head>
+    <meta charset = "UTF-8">
+    <title>Bugs</title>
+  </head>
+  <body>
+    <h1>I am here</h1>
+  </body>
