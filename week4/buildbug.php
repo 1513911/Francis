43dda88631ac4f2b04c3d2dee36b8516758a7cda
1513911 <1513911@rgu.ac.uk>
2016-04-10 21:32:28 +0100

    Update buildbug.php

--- week4/buildbug.php
+++ week4/buildbug.php
@@ -2,13 +2,19 @@
 <?php 
 
 include("connection.php");
+include("check.php");
+$sql1 = "SELECT userID FROM users WHERE username = '$user_check'";
+$result = mysqli_query($db,$sql1);
+$row = $result->fetch_array();
+$user = $row['userID']
+
 
 $title = $_POST['title'];
 $description = $_POST['description'];
 $postDate = $_POST['dateposted'];
 $fixedDate = $_POST['datefixed'];
 
-$sql = "INSERT INTO bugs(title, desc, postDate, fixDate) VALUES('$title','$description', '$postDate', '$fixedDate')";
+$sql = "INSERT INTO bugs(title, desc, postDate, fixDate, userID) VALUES('$title','$description', '$postDate', '$fixedDate', '$user')";
 mysqli_query($db,$sql);
  
 header("location:bugpostresponse.html");
